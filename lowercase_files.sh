46c4b54f7f7d6781a99d0ced8c096254cbb767ec
Nicolas Garcia <nicolas.garciaiii@gmail.com>
2024-09-15 21:34:04 -0500

    Create lowercase_files.sh

---
+++ lowercase_files.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# lowercase_files.sh
+# self-explanatory: lowercase all file names in directory
+
+# checks for a directory path
+if [ -z "$1" ]; then
+	echo "Provide a directory to lowercase files"
+	exit 1
+fi
+
+# switch to provided directory
+cd "$1" || exit 1
+
+# the main lowercasing script
+for file in *; do
+	# skip directories
+	if [ -f "$file" ]; then
+
+		# lowercase all
+		newfile=$(echo "$file" | tr '[:upper:]' '[:lower:]')
+
+		# rename the changed files
+		if [ "$file" != "$newfile" ]; then
+			mv -- "$file" "$newfile"
+		fi
+	fi
+done
